65f6f9b9b7515e738c8935981731ef8e4f3ac84d
dheeraj0274 <136884023+dheeraj0274@users.noreply.github.com>
2025-06-08 10:29:18 +0530

    Fix merge conflict

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const serviceRoutes = require('./routes/ServiceRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 
-dotenv.config({path: './config/.env'}); // Load environment variables
+
+
 
 connectDB();
 
@@ -23,8 +24,8 @@ app.use('/api/users', userRoutes);
 app.use('/api/providers', providerRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api/auth', authRoutes); 
-const otp= Math.floor(100000 + Math.random()* 900000);
-console.log(`Your OTP is: ${otp}`); // This is just for demonstration; in production, you would send this via SMS or email
+
+
 
   
 const PORT = process.env.PORT || 5000;
